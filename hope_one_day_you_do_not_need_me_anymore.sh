2e71a9e51bc0d8303677c0fa0470c4565865e68a
Qiyuan Pu <pqy7172@gmail.com>
2021-04-18 19:48:31 +0800

    complete of trans

---
+++ hope_one_day_you_do_not_need_me_anymore.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+switch=`ps aux |grep zathura |grep -v grep`
+
+if [ -z "$switch" ]
+then
+    :
+else
+
+    trans_res=/tmp/res
+    tidy=/tmp/tidy
+
+    word=`xsel -b |sed -n "1,1p"`
+    $HOME/trans_tools/trans.sh en:zh-CN "$word" 2>/dev/null  |cat -n > ${trans_res}
+    locate_line=`grep -e "English" ${trans_res} | awk -F " " '{print $1}'`
+    line=$(($locate_line-4))
+    sed -n "${line},${line}p" ${trans_res}  |awk -F " " '{print $2}' >${tidy}
+
+    really_token=`tail -c +5 ${tidy} | head -c -6`
+    notify-send --icon=info `echo $really_token`
+fi
+
+
