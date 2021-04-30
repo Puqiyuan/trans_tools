887116d5c5bea8715cf48693339ef02c948def97
Qiyuan Pu <pqy7172@gmail.com>
2021-04-30 23:26:02 +0800

    no need limit to zathura only

--- hope_one_day_you_do_not_need_me_anymore.sh
+++ hope_one_day_you_do_not_need_me_anymore.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-switch=`ps aux |grep zathura |grep -v grep`
+trans_res=/tmp/res
+tidy=/tmp/tidy
 
-if [ -z "$switch" ]
-then
-    :
-else
+word=`xsel -b |sed -n "1,1p"`
+$HOME/trans_tools/trans.sh en:zh-CN "$word" 2>/dev/null  |cat -n > ${trans_res}
+locate_line=`grep -e "English" ${trans_res} | awk -F " " '{print $1}'`
+line=$(($locate_line-4))
+sed -n "${line},${line}p" ${trans_res}  |awk -F " " '{print $2}' >${tidy}
 
-    trans_res=/tmp/res
-    tidy=/tmp/tidy
-
-    word=`xsel -b |sed -n "1,1p"`
-    $HOME/trans_tools/trans.sh en:zh-CN "$word" 2>/dev/null  |cat -n > ${trans_res}
-    locate_line=`grep -e "English" ${trans_res} | awk -F " " '{print $1}'`
-    line=$(($locate_line-4))
-    sed -n "${line},${line}p" ${trans_res}  |awk -F " " '{print $2}' >${tidy}
-
-    really_token=`tail -c +5 ${tidy} | head -c -6`
-    notify-send --icon=info `echo $really_token`
-fi
-
-
+really_token=`tail -c +5 ${tidy} | head -c -6`
+notify-send --icon=info `echo $really_token`
